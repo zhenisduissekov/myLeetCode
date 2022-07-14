09180fe9f061fc2511b661a4ace7a210a69fe15e
zhenis duissekov <52789114+zhenisduissekov@users.noreply.github.com>
2022-07-14 12:06:54 +0300

    Create 0141_LinkedListCycleTwoPointers.go

---
+++ 0141_LinkedListCycleTwoPointers.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+    if head == nil || head.Next == nil {
+        return false
+    }
+    
+    p1, p2 := head, head.Next
+    
+    for p1 != p2 {
+        if p1 == nil || p1.Next == nil {
+            return false
+        }
+        
+        p1 = p1.Next
+        p2 = p2.Next.Next
+    }
+    return true
+}
