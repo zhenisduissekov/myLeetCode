ddd4631c0e5861214c08a3c38b57fadce8d4d405
zhenis duissekov <52789114+zhenisduissekov@users.noreply.github.com>
2023-04-10 09:46:57 +0300

    Update problem20_20. ValidParentheses.go

--- problem20_20. ValidParentheses.go
+++ problem20_20. ValidParentheses.go
@@ -14,3 +14,58 @@ func isValid(s string) bool {
 	}
 	return len(openBrackets) == 0
 }
+
+
+//solution 2 with Stack
+func isValid(s string) bool {
+    closing := map[rune]rune{')': '(',']': '[', '}': '{'}
+    st := New()
+	for _, elem := range(s) {
+        if val, ok := closing[elem]; ok {
+            top := st.Pop()
+            if top == nil || top != val{
+                return false
+            }
+        } else {
+            st.Push(elem)
+        }
+    }
+    
+    return st.len == 0
+}
+
+
+type stack struct {
+    top *node
+    len int
+}
+
+type node struct {
+    val interface{}
+    prev *node
+}
+func New() *stack{
+    n := &node{nil, nil}
+    return &stack{
+        top: n,
+        len: 0,
+    }
+}
+
+func (s *stack) Push(val interface{}) {
+    n := &node{val: val, prev: s.top }
+    s.len ++
+    s.top = n
+}
+
+func (s *stack) Pop() interface{} {
+    if s.len == 0 {
+        return nil
+    }
+    n := s.top
+    s.len --
+    s.top = n.prev
+    return n.val
+}
+
+
