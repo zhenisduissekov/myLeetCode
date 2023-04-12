5584bbc30670402f8c1223fdda8f9251696bda5b
zhenis duissekov <52789114+zhenisduissekov@users.noreply.github.com>
2023-04-12 05:48:40 +0300

    Create problem21_MergeTwoSortedLists.go

---
+++ problem21_MergeTwoSortedLists.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+    var l3 ListNode
+    tail := &l3
+    for l1 != nil && l2 != nil {
+        if l1.Val < l2.Val {
+            tail.Next= l1
+            l1 = l1.Next
+        } else {
+            tail.Next = l2
+            l2 = l2.Next
+        }
+        tail = tail.Next
+    }
+    if l1 != nil {
+        tail.Next = l1
+    }
+    if l2 != nil {
+        tail.Next = l2
+    }
+    
+    return l3.Next
+}
