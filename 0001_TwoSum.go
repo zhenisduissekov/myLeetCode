6d9efc6b3566e3199c1736a9c5500df9630dbb28
ZHENIS DUISSEKOV <52789114+zhenisduissekov@users.noreply.github.com>
2025-03-04 10:58:54 +0300

    Update 0001_TwoSum.go

--- 0001_TwoSum.go
+++ 0001_TwoSum.go
@@ -1,3 +1,4 @@
+// I way
 func twoSum(nums []int, target int) []int {
     for i:=0;i<len(nums);i++ {
         for j:=i+1;j<len(nums);j++ {
@@ -7,4 +8,21 @@ func twoSum(nums []int, target int) []int {
         }
     }
     return []int{0, 0}
+}
+
+//II way
+func twoSum(nums []int, target int) []int {
+	slices.Sort(nums)
+	i, j := 0, len(nums)-1
+	for i < j {
+		switch {
+		case nums[i]+nums[j] > target:
+			j--
+		case nums[i]+nums[j] < target:
+			i++
+		case nums[i]+nums[j] == target:
+			return []int{i, j}
+		}
+	}
+	return []int{}
 }
