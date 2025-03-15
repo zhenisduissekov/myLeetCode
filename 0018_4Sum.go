691bad7312a78c166f8ecae200f87e2ff85b62fc
ZHENIS DUISSEKOV <52789114+zhenisduissekov@users.noreply.github.com>
2025-03-15 15:50:35 +0300

    Update 0018_4Sum.go

--- 0018_4Sum.go
+++ 0018_4Sum.go
@@ -32,3 +32,47 @@ func fourSum(nums []int, target int) [][]int {
     }
     return result
 }
+
+
+// без хеш мапы
+
+func fourSum(nums []int, target int) [][]int {
+    sort.Ints(nums)
+    n := len(nums)
+
+    result := make([][]int,0)
+    
+    for i:=0;i<n-3;i++ {
+        if i>0 && nums[i] == nums[i-1] {
+            continue
+        }
+       for j:=i+1;j<n-2;j++ {
+        if j>i+1 && nums[j]==nums[j-1] {
+            continue
+        }
+        k:=j+1
+        m:=n-1
+        for k<m {
+            switch {
+            case nums[i]+nums[j]+nums[k]+nums[m] == target:
+                result = append(result, []int{nums[i], nums[j], nums[k], nums[m]})
+
+                for k<m && nums[k]==nums[k+1] {
+                    k++
+                }
+                k++
+                for k<m && nums[m]==nums[m-1] {
+                    m--
+                }
+               m--
+
+            case nums[i]+nums[j]+nums[k]+nums[m] > target:
+                m--
+            default:
+                k++
+            }
+        }
+       }    
+    }
+    return result
+}
